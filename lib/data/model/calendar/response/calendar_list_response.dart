d543c2447a19a1741f6679c89cce7621511cdc96
leewoooo <leewoooo.dev@gmail.com>
2022-12-28 00:03:09 +0900

    🔨Update: add toLinkedMap method

--- lib/data/model/calendar/response/calendar_list_response.dart
+++ lib/data/model/calendar/response/calendar_list_response.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:pyc/data/model/calendar/response/calendar_response.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarListResponse {
   final List<CalendarResponse> rows;
@@ -12,4 +15,13 @@ class CalendarListResponse {
   CalendarListResponse.fromJSON(Map<String, dynamic> json)
       : rows = (json['rows'] as List<dynamic>).map((e) => CalendarResponse.fromJSON(e)).toList(),
         count = json['count'];
+
+  // TODO: 이벤트 불러와서 Map으로 만든 후 제공
+  LinkedHashMap<DateTime, List<CalendarListResponse>> toLinkedMap(DateTime day) {
+    return LinkedHashMap<DateTime, List<CalendarListResponse>>(equals: isSameDay, hashCode: _getHashCode);
+  }
+
+  int _getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
 }
